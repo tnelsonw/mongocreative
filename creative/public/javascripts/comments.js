442eb6b70113d99abd17044c9bb4bd6551caafff
Nelson Whitney <nelsoccer77@yahoo.com>
2017-11-15 18:55:05 +0000

    changes

--- creative/public/javascripts/comments.js
+++ creative/public/javascripts/comments.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
       var everything = "<ul>";
       for(var comment in data) {
         com = data[comment];
-        everything += "<li> Name: " + com.Name + " -- Comment: " + com.Comment + "</li>";
+        everything += "<li>" + com.Name + "<br> <img src=\"" + com.Comment + "\" alt=\"\"></li>";
       }
       everything += "</ul>";
       $("#comments").html(everything);
